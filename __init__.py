3c57328407357c12666682ee98e01793a1da098a
Alex Bortok <dev@bortok.com>
2022-10-05 09:06:16 -0700

    flask app.config with ROOT

--- __init__.py
+++ __init__.py
@@ -5,18 +5,22 @@ from flask import Flask, escape, request
 def create_app(config=None):
   # create and configure the app
   app = Flask(__name__, instance_relative_config=True)
-
-  # ensure the instance folder exists
-  try:
-      os.makedirs(app.instance_path)
-  except OSError:
-      pass
+  app.config.from_mapping(
+    ROOT='./clab',
+  )
+  
+  if config is None:
+    # load the instance config, if it exists
+    app.config.from_pyfile('config.py', silent=True)
+  else:
+    # load the config if passed in
+    app.config.from_mapping(config)
 
   from . import clab
   # URL handler
   @app.route('/collect/clab/<topology>/nodes/')
   def app_clab_node_data(topology):
     t = escape(topology)
-    return clab.get_clab_node_data('./clab', t) # run flask from the parent directory
+    return clab.get_clab_node_data(app.config['ROOT'], t)
 
   return app
